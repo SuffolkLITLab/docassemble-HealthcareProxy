273ca3be535f49016186fd0f2dc76dc2869c642c
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-04-21 14:15:16 -0400

    Migrate to ALDocument and add addendum/overflow for exceptions

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.HealthcareProxy',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.GuiltyPlea001'],
+      install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/HealthcareProxy/', package='docassemble.HealthcareProxy'),
      )
